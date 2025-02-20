e7c6dc766fafd55315b2f374ebf40b26d5c8989e
Liza Siddiqui <lizasiddiqui69@gmail.com>
2025-02-20 17:08:44 +0500

    Add files via upload

---
+++ assignment3/app.js
@@ -0,0 +1,56 @@
+// var city="karachi";
+// if(city=="karachi"){
+//     console.log("THE city of light");
+// }
+
+var x=2,y=2;
+if(x===y){
+    var z=+prompt("enter value of z");
+    console.log(z);
+}
+
+
+var zip_code=+prompt("enter your zip code");
+if(zip_code===10010){
+    alert("karachi");
+}
+else{
+    alert("rewrite correct code");
+}
+
+var num = 7;
+if(typeof num==="number"){
+num=1;
+console.log(num);
+}
+
+var x=2,y=2;
+if(x!=y){
+    var z=+prompt("enter value of z");
+    console.log(z);
+}
+
+var x=2,y=2;
+if(x>=y){
+    var z=+prompt("enter value of z");
+    console.log(z);
+}
+
+var x=2,y=2;
+if(x!=y){
+  alert("congratulations!");
+}
+
+var city = prompt("Please enter a city:");
+
+if (city === "Karachi") {
+    alert("This is Karachi.");
+} 
+else if (city === "Lahore") 
+    {
+    alert("This is Lahore.");
+}
+ else {
+    alert("This is a different city.");
+}
+
